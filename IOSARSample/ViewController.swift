92d30766f21cee4d9e15e00d3836a6783b7e28a1
Hai Pham <swiften.svc@gmail.com>
2019-05-16 02:54:58 +0800

    Initial Commit

---
+++ IOSARSample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  IOSARSample
+//
+//  Created by Hai Pham on 16/5/19.
+//  Copyright © 2019 swiften. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
