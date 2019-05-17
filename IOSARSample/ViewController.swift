90da7232b873972edfab59b18da84fb6f34c0514
Viet Hai Pham <viethai.pham@grabtaxi.com>
2019-05-18 01:43:33 +0800

    feat(ar): remove QR reader code to prepare for something else

--- IOSARSample/ViewController.swift
+++ IOSARSample/ViewController.swift
@@ -9,25 +9,8 @@
 import ARKit
 import UIKit
 
-extension SCNMaterial {
-  convenience init(color: UIColor) {
-    self.init()
-    diffuse.contents = color
-  }
-  
-  convenience init(image: UIImage) {
-    self.init()
-    diffuse.contents = image
-  }
-}
-
 public final class ViewController: UIViewController {
   @IBOutlet private weak var sceneKitView: ARSCNView!
-  
-  private lazy var qrScanner: QRScanner = {
-    let detector = CIDetector.init(ofType: CIDetectorTypeQRCode, context: nil, options: nil)!
-    return QRScanner(detector)
-  }()
   
   private lazy var lastCallTime: TimeInterval = 0
   private lazy var lock: NSLock = NSLock()
@@ -47,23 +30,6 @@ public final class ViewController: UIViewController {
     super.viewWillDisappear(animated)
     self.sceneKitView.session.pause()
   }
-  
-  private func calculateScene(_ response: QRResponse) -> SCNScene {
-    let arrowPath = UIBezierPath.arrow(fromTip: response.position.twoDPoint(),
-                                       length: 0.1,
-                                       radianFromTip: 0.785398,
-                                       tailWidth: 0.05,
-                                       headWidth: 0.1,
-                                       headLength: 0.05)
-
-    let shape = SCNShape(path: arrowPath, extrusionDepth: 0)
-    let arrowNode = SCNNode(geometry: shape)
-    arrowNode.position = response.position
-
-    let scene = SCNScene()
-    scene.rootNode.addChildNode(arrowNode)
-    return scene
-  }
 }
 
 // MARK: - ARSessionDelegate
@@ -77,10 +43,5 @@ extension ViewController: ARSessionDelegate {
     let currentTime = Date().timeIntervalSince1970
     guard currentTime - self.lastCallTime > 1 else { return }
     self.lastCallTime = currentTime
-    self.sceneKitView.scene.rootNode.childNodes.forEach({$0.removeFromParentNode()})
-
-    self.qrScanner.findQR(in: frame).forEach({
-      self.sceneKitView.scene = self.calculateScene($0)
-    })
   }
 }
